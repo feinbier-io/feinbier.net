b2e5da5cb010f0450f9a2236838409c3cc3be8c5
Michael Feinbier <mf@tiberianmaster.net>
2010-12-07 23:48:14 +0100

    Initialer Checkin

---
+++ js/user.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	$('#infobox, a.button').buttonHover({
+		delay: 150
+	});
+	
+	$('#footer a').fancybox({
+		'overlayShow'	:	false
+	});
+});
+
+(function($){
+	$.fn.buttonHover = function(options) {
+		/** Fade Settings **/
+		var settings = {
+			fadeTo: 0.5,
+			delay: 300
+		};
+	
+		return this.each(function() {
+			settings = $.extend(settings, options);
+		
+			$(this).fadeTo(0, settings.fadeTo).hover(
+				function() { $(this).fadeTo(settings.delay, 1); },
+				function() { $(this).fadeTo(settings.delay, settings.fadeTo); }
+			);
+			
+			return this;
+		});
+	};
+})(jQuery);
