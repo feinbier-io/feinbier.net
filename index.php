fae25f4fcdbbea2e07990614ce799857b669759a
Michael Feinbier <michael@feinbier.net>
2014-10-01 23:41:59 +0200

    Content Updates

--- index.php
+++ index.php
@@ -13,23 +13,29 @@
     <link rel="import" href="elements/feinbier-page.html">
     <link rel="import" href="elements/feinbier-footer.html">
     <link rel="import" href="bower_components/core-media-query/core-media-query.html">
+    <link rel="import" href="bower_components/paper-fab/paper-fab.html">
 
 </head>
 <body fullbleed>
     <feinbier-page>
         <div layout horizontal class="content-section dark intro">
 	        <figure flex>
+                <span class="flaticon-circuit4"></span>
 		        <?php #echo file_get_contents('images/lego3.svg'); ?>
 	        </figure>
-	        <div flex>
+	        <div flex three>
 		        <h3>Herzlich Willkommen</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam corporis dolore dolorum enim eveniet facilis hic incidunt inventore,
-		        ipsum, magni nam nostrum, quidem ratione rerum tempore vel voluptatum. Animi commodi debitis distinctio
-		        exercitationem minima nam praesentium provident quasi quia, sed? Cumque deserunt explicabo harum pariatur quae quasi sed voluptatem.</p>
+				<p>Ich bin Michael Feinbier und ich mache was mit Medien. Gefangen als Rockstar im K&ouml;rper eines Informatikers besch&auml;ftige ich mich
+                seit nun &uuml;ber 10 Jahren mit den Themen <em>PHP, Symfony, TYPO3, Laravel</em> und <em>Sencha (Touch)</em>.
+                Dazu gekommen sind in den letzten Jahren Buzzwords wie <em>iOS &amp; Android-Entwicklung, HTML5, WebApps</em> und <em>Node.js</em>.
+                Das was Sie hier sehen, ist mit <em>Polymer</em> realisiert.
+                "Aufgewachsen" bin ich in der Werbung und habe f&uuml;r Kunden wie die Deutsche Post, Praktiker, Johnson&amp;Johnson, O<sub>2</sub> oder BMW gearbeitet.<br>
+                Auf den folgenden Seiten m&ouml;chte ich einen kleinen Einblick in Teile meiner Arbeit geben.</p>
 	        </div>
         </div>
 
 	    <div class="content-section light">
+            <paper-fab raisedbutton icon="arrow-drop-down"></paper-fab>
 		    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, libero!</p>
 	    </div>
 
